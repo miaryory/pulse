8235dc96ab01c7812e5224b451bc6e0fd51637d3
Miary M. Ory <miar0004@stud.kea.dk>
2021-12-27 22:22:49 +0100

    footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Footer.module.css';
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 export default function Footer(){
     return(
@@ -6,20 +7,26 @@ export default function Footer(){
             <div className={styles.footerContainer}>
                 <div>
                     <p className={styles.footerHeadear}>PULSE</p>
-                    <p>Adress</p>
-                    <p>Phone number</p>
+                    <p>MIARY ORY</p>
+                    <p>WEB DEVELOPMENT</p>
+                    <p>KEA</p>
                 </div>
 
                 <div>
                     <p className={styles.footerHeadear}>HELP</p>
                     <p>FAQ</p>
                     <p>SUPPORT</p>
+                    <p>CONTACT US</p>
                 </div>
 
                 <div>
                     <p className={styles.footerHeadear}>FOLLOW US</p>
-                    <p>Facebook</p>
-                    <p>Instagram</p>
+                    <p>
+                        <FaFacebook size={25} color={"grey"}/>
+                    </p>
+                    <p>
+                        <FaInstagram size={25} color={"grey"}/>
+                    </p>
                 </div>
             </div>
         </footer>
