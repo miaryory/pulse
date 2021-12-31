b5000d6e2929dfd7b6ef2bece305acd50a5069a8
Miary M. Ory <miar0004@stud.kea.dk>
2021-12-31 21:53:02 +0100

    image size

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ module.exports = {
   images: {
     domains: ['miaryory.com'],
     minimumCacheTTL: 60,
-    imageSizes: [16, 96, 384],
-    deviceSizes: [640, 1080, 1920],
   },
   reactStrictMode: true,
 }
